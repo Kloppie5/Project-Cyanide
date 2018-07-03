bdd5c04622d39b106da94d79aa3cf7486f181d68
Kloppie5 <jeroen.kloppenburg@xs4all.nl>
2018-07-03 21:49:14 +0200

    Added process detection and drawing update on drag.
    [!] Memory leak

--- Project-Cyanide/Program.cs
+++ Project-Cyanide/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -10,6 +11,7 @@ using System.Windows.Forms;
 namespace Project_Cyanide {
 	class Program {
 		static void Main( string[] args ) {
+			Console.WriteLine("Program started on {0:d} at {0:t}", DateTime.Now);
 			Overlay overlay = new Overlay();
 			Application.Run(overlay);
 		}
@@ -28,16 +30,24 @@ namespace Project_Cyanide {
 		private static extern bool SetForegroundWindow( IntPtr hWnd );
 		[DllImport("user32.dll")]
 		[return: MarshalAs(UnmanagedType.Bool)]
-		static extern bool GetWindowRect( IntPtr hWnd, ref Rectangle lpRect );
+		static extern bool GetWindowRect( IntPtr hWnd, ref RECT lpRect );
 		[DllImport("user32.dll")]
 		private static extern IntPtr SetWinEventHook( uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags );
 
+		[StructLayout(LayoutKind.Sequential)]
+		public struct RECT {
+			public int Left;
+			public int Top;
+			public int Right;
+			public int Bottom;
+		}
+
 		protected WinEventDelegate _WinEventDelegate;
 
-		public Overlay() {
-			// _WinEventDelegate = new WinEventDelegate(WinEventCallback);
+		private List<Process> _processes;
 
-			// SetWinEventHook(0x800B, 0x800B, IntPtr.Zero, _WinEventDelegate, (uint) Id, GetWindowThreadProcessId(MainWindowHandle, IntPtr.Zero), 0x3);
+		public Overlay() {
+			_WinEventDelegate = new WinEventDelegate(WinEventCallback);
 
 			Rectangle rect = Screen.PrimaryScreen.Bounds;
 			TransparencyKey = Color.Turquoise;
@@ -51,12 +61,27 @@ namespace Project_Cyanide {
 
 			SetForegroundWindow(Handle);
 
-			Console.WriteLine("");
+			_processes = new List<Process>();
+
+			Console.WriteLine("Initialized Overlay");
+
+			foreach ( Process process in Process.GetProcesses() ) {
+				AddProcess(process);
+			}
 		}
 
-		// protected void WinEventCallback( IntPtr hWinEventHook, uint eventType, IntPtr hWnd, uint idObject, long idChild, uint dwEventThread, uint dwmsEventTime ) {
-		//	
-		// }
+		public void AddProcess( Process process ) {
+			if ( process.MainWindowHandle != IntPtr.Zero ) {
+				_processes.Add(process);
+				SetWinEventHook(0x800B, 0x800B, IntPtr.Zero, _WinEventDelegate, (uint) process.Id, GetWindowThreadProcessId(process.MainWindowHandle, IntPtr.Zero), 0x3);
+				Console.WriteLine("Added process {0}: {1} with Handle {2}", process.Id, process.ProcessName, process.MainWindowHandle);
+			}
+		}
+
+		protected void WinEventCallback( IntPtr hWinEventHook, uint eventType, IntPtr hWnd, uint idObject, long idChild, uint dwEventThread, uint dwmsEventTime ) {
+			if ( eventType == 0x800B && idObject == 0 )
+				Invalidate();
+		}
 
 		protected override void OnLoad( EventArgs e ) {
 			base.OnLoad(e);
@@ -72,12 +97,18 @@ namespace Project_Cyanide {
 			Graphics g = Graphics.FromImage(img);
 
 			DrawSquareFrame(g, Color.LightBlue, 3f, 0, 0, Size.Width, Size.Height);
+			Random r = new Random();
+			foreach ( Process process in _processes ) {
+				RECT rect = new RECT();
+				if ( GetWindowRect(process.MainWindowHandle, ref rect) )
+					DrawSquareFrame(g, Color.Green, 2f, rect.Left, rect.Top, rect.Right, rect.Bottom);
+			}
 
 			display.DrawImage(img, ClientRectangle);
 			img.Dispose();
 		}
 
-		public void DrawSquareFrame(Graphics g, Color color, float width, int x1, int y1, int x2, int y2) {
+		public void DrawSquareFrame( Graphics g, Color color, float width, int x1, int y1, int x2, int y2 ) {
 			Pen pen = new Pen(color, width);
 				g.DrawLine(pen, x1, y1, x2, y1);
 				g.DrawLine(pen, x1, y1, x1, y2);

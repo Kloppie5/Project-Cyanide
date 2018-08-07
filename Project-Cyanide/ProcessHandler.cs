76fd180a7329c09bd80e665e58e2edf0e3586de4
Kloppie5 <jeroen.kloppenburg@xs4all.nl>
2018-08-08 01:47:24 +0200

    Added ProcessHandler
    Moved Overlay creation into new thread
    Added basic example actions for Crusaders of the Lost Idols

---
+++ Project-Cyanide/ProcessHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Project_Cyanide {
+	class ProcessHandler {
+
+		private Process _process;
+
+		private Dictionary<String, Action<Process>> _actions;
+
+		public ProcessHandler( Process process ) {
+			_actions = new Dictionary<string, Action<Process>>();
+
+			_process = process;
+		}
+
+		public Boolean InvokeAction( String name ) {
+			if ( !_actions.TryGetValue(name, out Action<Process> action) )
+				return false;
+
+			action.Invoke(_process);
+			return true;
+		}
+		public void AddAction( String name, Action<Process> action ) {
+			_actions.Add(name, action);
+		}
+	}
+}
